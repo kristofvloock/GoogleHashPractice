34aa562ff7e60805458e81b35c16fc91a83566ab
Kristof Van Loock <kristof.vanloock@skynet.be>
2019-02-28 01:21:05 +0100

    Implemted solution

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-f = open('a_example.in', 'r')
+f = open('b_small.in', 'r')
 
 solution = []
+check = set()
 
 vars = f.readline().split(' ')
 R = int(vars[0])
@@ -12,8 +13,9 @@ H = int(vars[3].strip('\n'))
 
 sum_min = L
 sum_max = H - L
-
-types = {0: (14, 1), 1: (7, 2), 2: (2, 7), 3: (1, 14), 4: (12, 1), 5: (6, 2), 6: (4, 3), 7: (3, 4), 8: (2, 6), 9: (1, 12)}
+#types = {0: (12, 1), 1: (6, 2), 2: (4, 3), 3: (3, 4), 4: (2, 6), 5: (1, 12), 6: (10, 1), 7: (5, 2), 8: (2, 5), 9: (1, 10), 10: (8, 1), 11: (4, 2), 12: (2, 4), 13: (1, 8)}
+#types = {0: (14, 1), 1: (7, 2), 2: (2, 7), 3: (1, 14), 4: (12, 1), 5: (6, 2), 6: (4, 3), 7: (3, 4), 8: (2, 6), 9: (1, 12)}
+types = {0: (5, 1), 1: (1, 5), 2: (4, 1), 3: (1, 4), 4: (3, 1), 5: (1, 3), 6: (2, 1), 7: (1, 2)}
 
 pizza = [[char for char in line.strip('\n')] for line in f.readlines()]
 
@@ -26,10 +28,18 @@ for i in range(len(pizza)):
 
 pizza = np.array(pizza)
 
+f.close()
 
-def isValid(x1, y1, x2, y2):
-    section = pizza[x1:x2+1,y1:y2+1]
+
+def isValid(x1, y1, x2, y2, btype):
+    if (x1, y1, x2, y2, btype) in check:
+        return False
+    if x2 > C or y2 > R:
+        return False
+    section = pizza[y1:y2+1, x1:x2+1]
     sum = np.sum(section)
+    #if (sum_min <= sum <= sum_max) and (section.max() < 2):
+        #print(x1, y1, x2, y2, "Block is valid")
     return (sum_min <= sum <= sum_max) and (section.max() < 2)
 
 
@@ -43,41 +53,72 @@ def isReady():
 def startPoint():
     for y in range(R):
         for x in range(C):
-            if pizza[x,y] != 2:
+            if pizza[y,x] < 2:
+                print(x,y)
                 return (x,y)
     return None
 
 
 def placeBlock(x1, y1, x2, y2, btype):
-    pizza[x1:x2+1, y1:y2+1] + 2
+    #print("placing block", x1, y1 ,x2 , y2, btype)
+    pizza[y1:y2+1, x1:x2+1] += 2
     solution.append([x1, y1, x2, y2, btype])
+    check.add((x1, y1, x2, y2, btype))
+    #print(pizza[:25, :25])
+
 
 
 def deleteBlock():
-    x1, x2, y1, y2, btype = solution[-1]
-    pizza[x1:x2 + 1, y1:y2 + 1] - 2
+    x1, y1, x2, y2, btype = solution[-1][:]
+    #print("deleting block", x1, y1 ,x2 , y2, btype)
+    pizza[y1:y2 + 1, x1:x2 + 1] -= 2
+
     del solution[-1]
 
 
 def backtrack():
-    x1, x2, y1, y2, btype = solution[-1]
+    if len(solution) > 5:
+        print(solution[-5:])
+    #print("Backtracking...")
+    print(len(solution))
+    if len(solution) == 286:
+        print(solution[285])
+        print(pizza[:15, 590:610])
+        exit()
+    #print(pizza[48:70, 50:70])
+    try:
+        x1, y1, x2, y2, btype = solution[-1]
+    except:
+        print(pizza[:25, :25])
+        exit()
     deleteBlock()
     for newtype in range(btype+1, len(types)):
-        if isValid(x, y, types[btype][0]+x-1, types[btype][1]+y-1):
-            placeBlock(x, y, types[btype][0]+x-1, types[btype][1]+y-1)
+        if isValid(x1, y1, types[newtype][0]+x1-1, types[newtype][1]+y1-1, newtype):
+            #print("found new valid block")
+            placeBlock(x1, y1, types[newtype][0]+x1-1, types[newtype][1]+y1-1, newtype)
             return
     backtrack()
+    return
 
 
 while not isReady():
     x, y = startPoint()
+    #print("start point:", x,y)
     isPlaced = False
-    for btype in types.keys():
-        if isValid(x, y, types[btype][0]+x-1, types[btype][1]+y-1):
-            placeBlock(x, y, types[btype][0]+x-1, types[btype][1]+y-1)
+    for btype in range(len(types)):
+        #print("btype", btype)
+        if isValid(x, y, types[btype][0]+x-1, types[btype][1]+y-1, btype):
+            placeBlock(x, y, types[btype][0]+x-1, types[btype][1]+y-1, btype)
             isPlaced = True
             break
     if not isPlaced:
         backtrack()
 
+f = open('solution.txt', 'w')
 
+f.write(str(len(solution))+'\n')
+for i in range(len(solution)):
+    for j in range(len(solution[i])-1):
+        f.write(str(solution[i][j])+ ' ')
+    f.write('\n')
+f.close()
