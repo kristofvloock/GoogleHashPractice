9f3fccefb25c88c2899da3563af02bbcbb70bcc2
Kristof Van Loock <kristof.vanloock@skynet.be>
2019-03-18 22:55:49 +0100

    Final Commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from time import sleep
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = open('b_small.in', 'r')
+f = open('c_medium.in', 'r')
 
 solution = []
 check = set()
@@ -14,9 +15,13 @@ H = int(vars[3].strip('\n'))
 
 sum_min = L
 
-#types = {0: (12, 1), 1: (6, 2), 2: (4, 3), 3: (3, 4), 4: (2, 6), 5: (1, 12), 6: (10, 1), 7: (5, 2), 8: (2, 5), 9: (1, 10), 10: (8, 1), 11: (4, 2), 12: (2, 4), 13: (1, 8)}
+# MEDIUM
+# types = {0: (12, 1), 1: (6, 2), 2: (4, 3), 3: (3, 4), 4: (2, 6), 5: (1, 12), 6: (10, 1), 7: (5, 2), 8: (2, 5), 9: (1, 10), 10: (8, 1), 11: (4, 2), 12: (2, 4), 13: (1, 8)}
+types = {0: (12, 1), 1: (11, 1), 2: (10, 1), 3: (9, 1), 4: (8, 1), 5: (6, 2), 6: (5, 2), 7: (4, 2), 8: (4, 3), 9: (3, 4), 10: (2, 4), 11: (2, 5), 12: (2, 6), 13: (1, 8), 14: (1, 9), 15: (1, 10), 16: (1, 11), 17: (1, 12)}
+# LARGE
 #types = {0: (14, 1), 1: (7, 2), 2: (2, 7), 3: (1, 14), 4: (12, 1), 5: (6, 2), 6: (4, 3), 7: (3, 4), 8: (2, 6), 9: (1, 12)}
-types = {0: (5, 1), 1: (1, 5), 2: (4, 1), 3: (1, 4), 4: (3, 1), 5: (1, 3), 6: (2, 1), 7: (1, 2)}
+# SMULL!
+# types = {0: (5, 1), 1: (1, 5), 2: (4, 1), 3: (1, 4), 4: (3, 1), 5: (1, 3), 6: (2, 1), 7: (1, 2)}
 
 pizza = [[char for char in line.strip('\n')] for line in f.readlines()]
 
@@ -35,7 +40,11 @@ f.close()
 def isValid(x1, y1, x2, y2, btype):
     #if (x1, y1, x2, y2, btype) in check:
     #    return False
-    sum_max = (x2-x1) * (y2-y1) - L
+    if x2 >= C or y2 >= R:
+        return False
+    # print(x1, y1, x2, y2, btype)
+    sum_max = (x2-x1+1) * (y2-y1+1) - L
+    # print("sum_max:", sum_max)
     if x2 > C or y2 > R:
         return False
     section = pizza[y1:y2+1, x1:x2+1]
@@ -62,30 +71,47 @@ def startPoint():
 
 
 def placeBlock(x1, y1, x2, y2, btype):
-    #print("placing block", x1, y1 ,x2 , y2, btype)
-    pizza[y1:y2+1, x1:x2+1] += 100
+    # print("placing block", x1, y1 ,x2 , y2, btype)
+    pizza[y1:y2+1, x1:x2+1] += (10+btype*5)
     solution.append([x1, y1, x2, y2, btype])
     check.add((x1, y1, x2, y2, btype))
+
+    
+
+    plt.imshow(pizza)
+    plt.colorbar()
+
+    plt.show(block=False)
+    plt.pause(.001)
+    plt.clf()
 
 
 
 
 def deleteBlock():
     x1, y1, x2, y2, btype = solution[-1][:]
-    print("deleting block", x1, y1 ,x2 , y2, btype)
-    print(pizza[(y1-5):(y2+5), (x1-5):(x2+5)])
-    pizza[y1:y2 + 1, x1:x2 + 1] -= 100
+    # print("deleting block", x1, y1 ,x2 , y2, btype)
+    # print(pizza[(y1-5):(y2+5), (x1-5):(x2+5)])
+    pizza[y1:y2 + 1, x1:x2 + 1] -= (10+btype*5)
 
     del solution[-1]
 
-
-def backtrack():
-    #print("Backtracking...")
-    print(len(solution))
-
     plt.imshow(pizza)
     plt.colorbar()
-    plt.show()
+
+    plt.show(block=False)
+    plt.pause(0.001)
+    plt.clf()
+
+
+
+def backtrack():
+    # print("Backtracking...")
+    # print(len(solution))
+
+    # plt.imshow(pizza)
+    # plt.colorbar()
+    # plt.show()
     try:
         x1, y1, x2, y2, btype = solution[-1]
     except:
@@ -114,9 +140,9 @@ while not isReady():
             placeBlock(x, y, types[btype][0]+x-1, types[btype][1]+y-1, btype)
             isPlaced = True
             break
-    plt.imshow(pizza)
-    plt.colorbar()
-    plt.show()
+    # plt.imshow(pizza)
+    # plt.colorbar()
+    # plt.show()
     if not isPlaced:
         backtrack()
 
@@ -129,3 +155,7 @@ for i in range(len(solution)):
     f.write('\n')
 f.close()
 
+plt.imshow(pizza)
+plt.colorbar()
+plt.show()
+print(len(solution))
